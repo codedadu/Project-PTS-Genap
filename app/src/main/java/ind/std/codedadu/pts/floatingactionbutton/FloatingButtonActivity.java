e5cd2d4bb47f221c4d683c7e9a22acd94c646c66
Asep Septiadi <mcreator.id.dev@gmail.com>
2019-02-21 22:24:17 +0700

    Update Project 21 Februari 2019 | Floating Action Button & Time Picker for get Time | Rev 1.7 by Asep Septiadi

--- app/src/main/java/ind/std/codedadu/pts/floatingactionbutton/FloatingButtonActivity.java
+++ app/src/main/java/ind/std/codedadu/pts/floatingactionbutton/FloatingButtonActivity.java
@@ -1,10 +1,13 @@
 package ind.std.codedadu.pts.floatingactionbutton;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.DatePicker;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
@@ -14,18 +17,19 @@ import java.util.Locale;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import ind.std.codedadu.pts.MainActivity;
 import ind.std.codedadu.pts.R;
 
 public class FloatingButtonActivity extends AppCompatActivity {
 
-    @BindView(R.id.fab_calendar)
-    FloatingActionButton fabCalendar;
 
     Calendar newCalendar;
     DatePickerDialog.OnDateSetListener setDate;
 
     String getDatefromAccess;
+    @BindView(R.id.fab_calendargetDate)
+    FloatingActionButton fabCalendargetDate;
+    @BindView(R.id.fab_calendargetTime)
+    FloatingActionButton fabCalendargetTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +51,15 @@ public class FloatingButtonActivity extends AppCompatActivity {
 
     }
 
-    @OnClick(R.id.fab_calendar)
-    public void onViewClicked() {
+    private void getDate() {
+        //String formatDateSQL = "yyyy/MM/dd";
+        String formatTanggalUmum = "dd, MMMM yyyy";
+        SimpleDateFormat sdfOfDate = new SimpleDateFormat(formatTanggalUmum, Locale.US);
+        String getDatefromAccess = sdfOfDate.format(newCalendar.getTime());
+        Toast.makeText(this, "Date get is set " + getDatefromAccess, Toast.LENGTH_SHORT).show();
+    }
+
+    public void method_getDate() {
         new DatePickerDialog(FloatingButtonActivity.this, setDate,
                 newCalendar.get(Calendar.YEAR),
                 newCalendar.get(Calendar.MONTH),
@@ -56,11 +67,32 @@ public class FloatingButtonActivity extends AppCompatActivity {
                 .show();
     }
 
-    private void getDate() {
-        //String formatDateSQL = "yyyy/MM/dd";
-        String formatTanggalUmum = "dd, MMMM yyyy";
-        SimpleDateFormat sdfOfDate = new SimpleDateFormat(formatTanggalUmum, Locale.US);
-        String getDatefromAccess = sdfOfDate.format(newCalendar.getTime());
-        Toast.makeText(this, "Date get is set " + getDatefromAccess, Toast.LENGTH_SHORT).show();
+    public void method_getTime() {
+        Calendar currentTime = Calendar.getInstance();
+        int hour = currentTime.get(Calendar.HOUR_OF_DAY);
+        int minute = currentTime.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog;
+        timePickerDialog = new TimePickerDialog(FloatingButtonActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int setHour, int setMinute) {
+                String getTimeset = setHour + ":" + setMinute;
+                Toast.makeText(FloatingButtonActivity.this, "Time set is " + getTimeset, Toast.LENGTH_SHORT).show();
+            }
+        }, hour, minute, true); // Set Time is 24 Hour
+        timePickerDialog.setTitle("Select Time");
+        timePickerDialog.show();
+    }
+
+    @OnClick({R.id.fab_calendargetDate, R.id.fab_calendargetTime})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.fab_calendargetDate:
+                method_getDate();
+                break;
+            case R.id.fab_calendargetTime:
+                method_getTime();
+                break;
+        }
     }
 }
